00a11a6edfe7191c01ec3b831378253b7f395258
la-yse <laysegabriellec@gmail.com>
2026-09-18 13:44:05 +0100

    Mudança nos cards da secção What I Do; adicionar language switch e index.html portugues.

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ const observer = new IntersectionObserver(
 revealElements.forEach((element) => {
     observer.observe(element);
 });
+
+
+//Language Toggle
+
+const languageOptions = document.querySelectorAll(".language-switch__option");
+const slider = document.querySelector(".language-switch__slider");
+
+languageOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        const language = option.dataset.lang;
+
+        if (language === "pt") {
+            window.location.href = "/pt/";
+        } else {
+            window.location.href = "/";
+        }
+    });
+});
+
+const currentLanguage = document.querySelector(
+    ".language-switch__option.active"
+)?.dataset.lang;
+
+if (currentLanguage === "pt") {
+    slider.style.transform = "translateX(31px)";
+}
